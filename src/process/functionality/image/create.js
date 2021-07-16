7c6ffcb48f34a4e499738463f417f291462d3920
FelixMZ2018 <FelixinLondon@googlemail.com>
2021-07-16 13:10:48 +0200

    PR Feedback

--- src/process/functionality/image/create.js
+++ src/process/functionality/image/create.js
@@ -2,12 +2,13 @@ const Amplify = require('aws-amplify')
 const serializeAxiosResponse = require('../../../utils/serializeAxiosResponse')
 const { DEFAULT_REQ_OPTS } = require('../../../utils/defaultReqOpts')
 /**
- * Uploads a process image, the image needs to be encoded in base64
+ * Uploads a process image
  * requires the image to be encoded in base64
  * and the file type as string
- * @param {string} ProcessId @param {string} FunctionalityID @param {object} Body
- * @example {imageData: ****, fileType: 'jpeg'}
- * @return {promise} Promise
+ * @async
+ * @param {string} ProcessId @param {string} FunctionalityId
+ * @param {object} Body {imageData: ****, fileType: 'jpeg'}
+ * @returns {Promise <object>} Promise resolving with the createdImageRef
  */
 module.exports = serializeAxiosResponse(
   (processId, functionalityId, body) => Amplify.API.post(
