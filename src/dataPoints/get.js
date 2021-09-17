29ff324467326e167c8fe638c541bc4f03278c86
Viktor Diezel <viktor.diezel@ospin.de>
2021-09-17 12:10:18 +0200

    fix: changes signature for datapoints GET

--- src/dataPoints/get.js
+++ src/dataPoints/get.js
@@ -4,5 +4,5 @@ const serializeAxiosResponse = require('../utils/serializeAxiosResponse')
 const { DEFAULT_REQ_OPTS } = require('../utils/defaultReqOpts')
 
 module.exports = serializeAxiosResponse(
-  params => API.get('datapoints', `processes/${params.processId}/functionalities/${params.reporterFctId}?numberOfPoints=${params.numberOfPoints}&start=${params.start}&end=${params.end}`, { ...DEFAULT_REQ_OPTS }),
+  (processId, reporterFctId, params) => API.get('datapoints', `processes/${processId}/functionalities/${reporterFctId}?numberOfPoints=${params.numberOfPoints}&start=${params.start}&end=${params.end}`, { ...DEFAULT_REQ_OPTS }),
 )
