c18ee4370c30ad35329da6886de67916710decbe
FelixMZ2018 <FelixinLondon@googlemail.com>
2021-09-23 16:56:47 +0200

    add `deviceAPI.devices.processes.get` Fixes #32

--- src/deviceAPI/process/get.js
+++ src/deviceAPI/process/get.js
@@ -1,7 +1,12 @@
 const serializeAxiosResponse = require('utils/serializeAxiosResponse')
 const { DEFAULT_REQ_OPTS } = require('utils/defaultReqOpts')
 const AuthorizedDeviceAPI = require('../AuthorizedDeviceAPI')
-
+/**
+ * Fetches information for a specific process
+ * @param processId the Id of the process to be fetched
+ * @async
+ * @returns {Promise <object>} Promise resolving to a serialized process object
+ */
 module.exports = serializeAxiosResponse(
   processId => AuthorizedDeviceAPI.get(`processes/${processId}`, { ...DEFAULT_REQ_OPTS }),
 )
