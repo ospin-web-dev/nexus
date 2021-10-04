d6ea384ea26a9cf505ecbd2a418dff4994c4a2da
Felix Menzel <36296555+FelixMZ2018@users.noreply.github.com>
2021-10-04 12:26:19 +0200

    feat: add commands​/devices​/device id/processes/process id/start process (#41)
    * added endpoint and test'
    
    * new endpoint call

--- src/command/device/process/updateRunningProcess.js
+++ src/command/device/process/updateRunningProcess.js
@@ -4,9 +4,9 @@ const serializeAxiosResponse = require('../../../utils/serializeAxiosResponse')
 const { DEFAULT_REQ_OPTS } = require('../../../utils/defaultReqOpts')
 
 module.exports = serializeAxiosResponse(
-  (deviceId, processId) => API.post(
+  (deviceId, processId, params) => API.post(
     'command',
-    `devices/${deviceId}/processes/${processId}/start-process`,
-    { body: {}, ...DEFAULT_REQ_OPTS },
+    `devices/${deviceId}/processes/${processId}/update-running-process`,
+    { body: params, ...DEFAULT_REQ_OPTS },
   ),
 )
