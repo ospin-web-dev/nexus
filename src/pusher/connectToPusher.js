f3ecf417c9b2b9270e6313e9f493112f6ea6c5d5
vdiezel <viktor.diezel@posteo.de>
2021-12-06 09:23:10 +0100

    removes API keys (should be injected by the client

--- src/pusher/connectToPusher.js
+++ src/pusher/connectToPusher.js
@@ -1,31 +1,16 @@
 const Pusher = require('pusher-js')
 const batchAuthorizer = require('./batchAuthorizer')
 
-const getAPIKey = env => {
-
-  const API_KEY_DEV = 'ba587719e2ccfb6c9dd1'
-  const API_KEY_STAGING = '10d4f25e41dc0f09c37d'
-  const API_KEY_PROD = 'ca9297d1e015925bf081'
-
-  const ENV_TO_API_KEY = {
-    dev: API_KEY_DEV,
-    staging: API_KEY_STAGING,
-    prod: API_KEY_PROD,
-  }
-
-  return ENV_TO_API_KEY[env] || API_KEY_DEV
-}
-
 let pusher = null
 
 const resetPusherClient = () => {
   pusher = null
 }
 
-const init = async (env, userId, badStateUpdateHandler = null) => {
+const init = async (apiKey, userId, badStateUpdateHandler = null) => {
   if (pusher) return pusher
 
-  pusher = new Pusher(getAPIKey(env), {
+  pusher = new Pusher(apiKey, {
     cluster: 'eu',
     authorizer: batchAuthorizer(userId),
   })
