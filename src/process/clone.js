fa422cc5557bfa116e538a7b231c1549b4cc8189
Felix Menzel <36296555+FelixMZ2018@users.noreply.github.com>
2021-12-07 15:47:20 +0100

    Update clone.js

--- src/process/clone.js
+++ src/process/clone.js
@@ -1,15 +1,7 @@
 const Amplify = require('aws-amplify')
 const serializeAxiosResponse = require('../utils/serializeAxiosResponse')
 const { DEFAULT_REQ_OPTS } = require('../utils/defaultReqOpts')
-/**
- * Uploads a process image
- * requires the image to be encoded in base64
- * and the file type as string
- * @async
- * @param {string} ProcessId @param {string} FunctionalityId
- * @param {object} Body
- * @returns {Promise <object>} Promise resolving with the createdImageRef
- */
+
 module.exports = serializeAxiosResponse(
   (processId, body) => Amplify.API.post(
     'process', `${processId}/clones`,
