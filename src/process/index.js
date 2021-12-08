6a46afecaaa051247e608d99b76321e65dcbf17f
Felix Menzel <36296555+FelixMZ2018@users.noreply.github.com>
2021-12-08 10:46:53 +0100

    feat: added clone process (#53)
    * added clone process
    
    * Update clone.js

--- src/process/index.js
+++ src/process/index.js
@@ -1,10 +1,12 @@
 const functionality = require('./functionality')
 const list = require('./list')
 const get = require('./get')
+const clone = require('./clone')
 const deleteMany = require('./deleteMany')
 
 module.exports = {
   functionality,
+  clone,
   list,
   get,
   deleteMany,
