64517a0f13691f786b23b36eae7f4aa0a2dae416
vdiezel <viktor.diezel@posteo.de>
2021-12-09 11:40:03 +0100

    feat: updates the test for the clone endpoint after rebase to use the new abstraction

--- test/src/process/clone.test.js
+++ test/src/process/clone.test.js
@@ -1,5 +1,6 @@
 const { API } = require('aws-amplify')
 const faker = require('faker')
+const testDefaultHTTPResponses = require('../../testHelpers/testDefaultHTTPResponses')
 
 const clone = require('process/clone')
 const { DEFAULT_REQ_OPTS } = require('utils/defaultReqOpts')
@@ -17,43 +18,5 @@ describe('post', () => {
     expect(API.post).toHaveBeenCalledWith('process', `${processId}/clones`, { body: { processName }, ...DEFAULT_REQ_OPTS })
   })
 
-  describe('on API.post success', () => {
-    beforeAll(() => {
-      jest.spyOn(API, 'post')
-        .mockImplementation((() => ({ data: 'success!', status: 200 })))
-    })
-
-    it('returns the serialized result', async () => {
-      const resp = await clone(processId)
-
-      expect(resp).toStrictEqual(expect.objectContaining({
-        success: true,
-        data: 'success!',
-        error: null,
-        errorMsg: null,
-        status: 200,
-      }))
-    })
-  })
-
-  describe('on API.post error', () => {
-    const error = new Error()
-
-    beforeAll(() => {
-      jest.spyOn(API, 'post')
-        .mockImplementation(() => { throw error })
-    })
-
-    it('returns a serialized error response', async () => {
-      const resp = await clone(processId)
-
-      expect(resp).toStrictEqual(expect.objectContaining({
-        success: false,
-        data: null,
-        error,
-        errorMsg: '',
-        status: null,
-      }))
-    })
-  })
+  testDefaultHTTPResponses(clone, 'post')
 })
