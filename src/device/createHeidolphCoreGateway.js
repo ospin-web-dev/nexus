130311080b8770d4f2ac684d3a5089fe6e4a7233
FelixMZ2018 <FelixinLondon@googlemail.com>
2022-05-06 09:20:41 +0200

    feat: add new core gateway endpoint

---
+++ src/device/createHeidolphCoreGateway.js
@@ -0,0 +1,8 @@
+const { default: API } = require('@aws-amplify/api-rest')
+
+const serializeAxiosResponse = require('../utils/serializeAxiosResponse')
+const { DEFAULT_REQ_OPTS } = require('../utils/defaultReqOpts')
+
+module.exports = serializeAxiosResponse(
+  params => API.post('device', 'heidolph-core-gateways', { body: params, ...DEFAULT_REQ_OPTS }),
+)
