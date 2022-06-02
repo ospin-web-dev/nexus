41bb31efe176c19c3055c3659ca5171de2030f87
FelixMZ2018 <FelixinLondon@googlemail.com>
2022-06-02 10:52:05 +0200

    removing unnecessary asyync

--- src/auth/globalSignOut.js
+++ src/auth/globalSignOut.js
@@ -3,5 +3,5 @@ const { default: Auth } = require('@aws-amplify/auth')
 const serializeAxiosResponse = require('../utils/serializeAxiosResponse')
 
 module.exports = serializeAxiosResponse(
-  async () => Auth.signOut({ global: true }),
+  () => Auth.signOut({ global: true }),
 )
