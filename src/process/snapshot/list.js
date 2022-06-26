e37fd96fa3f4ad13a4b3cb5b333bb920b724d650
Viktor Diezel <37934155+vdiezel@users.noreply.github.com>
2022-06-26 12:26:04 +0200

    feat: adds call to the new GET processes/:processId/snapshots endpoint (#92)

---
+++ src/process/snapshot/list.js
@@ -0,0 +1,7 @@
+const { default: API } = require('@aws-amplify/api-rest')
+const serializeAxiosResponse = require('../../utils/serializeAxiosResponse')
+const { DEFAULT_REQ_OPTS } = require('../../utils/defaultReqOpts')
+
+module.exports = serializeAxiosResponse(
+  ({ processId }) => API.get('process', `${processId}/snapshots`, DEFAULT_REQ_OPTS),
+)
