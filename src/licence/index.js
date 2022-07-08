5d2488570ac0b7a33e6411c50340e868f8f012ce
Viktor Diezel <viktor.diezel@ospin.de>
2022-07-08 15:06:30 +0200

    feat: adds call to update licence endpoint

--- src/licence/index.js
+++ src/licence/index.js
@@ -1,9 +1,11 @@
 const create = require('./create')
 const list = require('./list')
 const listTypes = require('./listTypes')
+const update = require('./update')
 
 module.exports = {
+  create,
   list,
   listTypes,
-  create,
+  update,
 }
