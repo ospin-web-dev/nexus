6355c7e38f4509f53d94f8c1bc40d04b6afc2eeb
FelixMZ2018 <FelixinLondon@googlemail.com>
2022-10-06 11:47:26 +0200

    condensing test files even more

--- test/src/device/functionalityGraph/ports/update.test.js
+++ test/src/device/functionalityGraph/ports/update.test.js
@@ -1,29 +1,21 @@
 const faker = require('faker')
-const { default: API } = require('@aws-amplify/api-rest')
 
 const updatePorts = require('device/functionalityGraph/ports/update')
 const { DEFAULT_REQ_OPTS } = require('utils/defaultReqOpts')
-const testDefaultHTTPResponses = require('../../../../testHelpers/testDefaultHTTPResponses')
 const testHTTPEndpoint = require('../../../../testHelpers/testHTTPEndpoint')
 
-describe('updatePorts', () => {
-  const params = {
-    deviceId: faker.datatype.uuid(),
-    fctGraphId: faker.datatype.uuid(),
-    body: {},
-  }
+const params = {
+  deviceId: faker.datatype.uuid(),
+  fctGraphId: faker.datatype.uuid(),
+  body: {},
+}
 
-  afterAll(() => { jest.restoreAllMocks() })
-
-  testHTTPEndpoint({
-    name: 'update physical mapping',
-    handler: updatePorts,
-    httpVerb: 'patch',
-    serviceName: 'device',
-    params: [...Object.values(params) ],
-    expectedURLSegment: `${params.deviceId}/functionality-graphs/${params.fctGraphId}/ports`,
-    expectedPayload: { body: {}, ...DEFAULT_REQ_OPTS },
-  })
-
-  testDefaultHTTPResponses(updatePorts, 'patch', [ params ])
+testHTTPEndpoint({
+  name: 'update physical mapping',
+  handler: updatePorts,
+  httpVerb: 'patch',
+  serviceName: 'device',
+  params: [...Object.values(params) ],
+  expectedURLSegment: `${params.deviceId}/functionality-graphs/${params.fctGraphId}/ports`,
+  expectedPayload: { body: {}, ...DEFAULT_REQ_OPTS },
 })
--- test/src/process/physicalMapping/update.test.js
+++ test/src/process/physicalMapping/update.test.js
@@ -1,25 +1,17 @@
-const { default: API } = require('@aws-amplify/api-rest')
 const faker = require('faker')
 
 const update = require('process/physicalMapping/update')
 const { DEFAULT_REQ_OPTS } = require('utils/defaultReqOpts')
-const testDefaultHTTPResponses = require('../../../testHelpers/testDefaultHTTPResponses')
 const testHTTPEndpoint = require('../../../testHelpers/testHTTPEndpoint')
 
-describe('update physicalMapping', () => {
-  const processId = faker.datatype.uuid()
+const processId = faker.datatype.uuid()
 
-  afterAll(() => { jest.restoreAllMocks() })
-
-  testHTTPEndpoint({
-    name: 'update physical mapping',
-    handler: update,
-    httpVerb: 'post',
-    serviceName: 'process',
-    params: [processId],
-    expectedURLSegment: `processes/${processId}/physical-mappings`,
-    expectedPayload: { body: {}, ...DEFAULT_REQ_OPTS },
-  })
-
-  testDefaultHTTPResponses(update, 'post', [ processId ])
+testHTTPEndpoint({
+  name: 'update physical mapping',
+  handler: update,
+  httpVerb: 'post',
+  serviceName: 'process',
+  params: [processId],
+  expectedURLSegment: `processes/${processId}/physical-mappings`,
+  expectedPayload: { body: {}, ...DEFAULT_REQ_OPTS },
 })
