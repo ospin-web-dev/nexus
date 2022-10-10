eea7e06511e0384071f39cb93d91ed250b7ba982
FelixMZ2018 <FelixinLondon@googlemail.com>
2022-10-10 15:14:04 +0200

    done

--- src/device/functionalityGraph/index.js
+++ src/device/functionalityGraph/index.js
@@ -1,12 +1,14 @@
 const configuration = require('./configuration')
 const physicalMapping = require('./physicalMapping')
 const ports = require('./ports')
+const update = require('./update')
 
 /**
  * @namespace nexus.device.functionalityGraph
  */
 
 module.exports = {
+  update,
   ports,
   configuration,
   physicalMapping,
