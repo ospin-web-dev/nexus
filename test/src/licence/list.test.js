af1f7f44a9e90affcb64ba505086b66999f68c2e
Viktor Diezel <37934155+vdiezel@users.noreply.github.com>
2022-12-01 22:28:26 +0100

    feat: allows passing query string parameters to LIST licences, devices and users (#124)

--- test/src/licence/list.test.js
+++ test/src/licence/list.test.js
@@ -2,9 +2,10 @@ const list = require('licence/list')
 const testHTTPEndpoint = require('../../testHelpers/testHTTPEndpoint')
 
 testHTTPEndpoint({
-  name: 'listTypes',
+  name: 'list',
   handler: list,
   httpVerb: 'get',
   serviceName: 'licence',
   params: [],
+  expectedPayload: { queryStringParameters: {} },
 })
