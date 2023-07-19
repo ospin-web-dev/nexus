2038efc2e5455481a92001d72a0980f943377c28
Lev Leontyev <Lev.Leontyev@ospin.de>
2023-07-19 14:45:01 +0200

    feat: metrics endpoint Fixes #155

---
+++ test/src/utilsService/getMetrics.test.js
@@ -0,0 +1,17 @@
+const { default: API } = require('@aws-amplify/api-rest')
+
+const getMetrics = require('utilsService/getMetrics')
+const { DEFAULT_REQ_OPTS } = require('utils/defaultReqOpts')
+const testDefaultHTTPResponses = require('../../testHelpers/testDefaultHTTPResponses')
+
+describe('getMetrics', () => {
+
+  it('calls amplify\'s API.get method', async () => {
+    jest.spyOn(API, 'get').mockImplementation(args => args)
+
+    await getMetrics()
+    expect(API.get).toHaveBeenCalledWith('utils', 'metrics', DEFAULT_REQ_OPTS)
+  })
+
+  testDefaultHTTPResponses(getMetrics, 'get')
+})
