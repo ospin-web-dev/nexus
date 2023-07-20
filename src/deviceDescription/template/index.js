4d520cb8c5a741e69f11f6d0a2534745e44c6425
Felix Menzel <36296555+FelixMZ2018@users.noreply.github.com>
2023-07-20 16:10:38 +0200

    feat: allow put template/layout (#154)
    * done
    
    * feat: add get template endpoint
    
    ---------
    
    Co-authored-by: Felix Menzel <Felix.Menzel.Dev@gmail.com>

--- src/deviceDescription/template/index.js
+++ src/deviceDescription/template/index.js
@@ -2,14 +2,17 @@ const create = require('./create')
 const list = require('./list')
 const remove = require('./remove')
 const update = require('./update')
-
+const putLayout = require('./putLayout')
+const get = require('./get')
 /**
  * @namespace nexus.deviceDescription.template
  */
 
 module.exports = {
+  putLayout,
   create,
   list,
   remove,
+  get,
   update,
 }
