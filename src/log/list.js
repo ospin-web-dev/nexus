241abecf891b0a2dae4ac166b8ccb43a6849966c
Nindya Hapsari <38020437+nindyahapsari@users.noreply.github.com>
2023-08-24 13:51:17 +0200

    feat: new method for fetching logs(#150) (#161)
    * wip
    
    * feat: new method for fetching logs
    
    * changes according to pr
    
    * delete utils func
    
    * changing docs pr
    
    * changes to doc pr

--- src/log/list.js
+++ src/log/list.js
@@ -2,7 +2,6 @@ const { default: API } = require('@aws-amplify/api-rest')
 
 const serializeAxiosResponse = require('../utils/serializeAxiosResponse')
 const { DEFAULT_REQ_OPTS } = require('../utils/defaultReqOpts')
-const { removeUndefinedKeys } = require('../utils/objUtils')
 
 /**
  * @desc queries the logs of a device
@@ -10,27 +9,13 @@ const { removeUndefinedKeys } = require('../utils/objUtils')
  * @function list
  * @async
  * @param {Object} params
- * @param {string} params.deviceId
- * @param {string} params.userId
- * @param {string} params.processId
- * @param {number} params.limit
- * @param {number} params.startTime
- * @param {number} params.endTime
+ * @param {Object} queryStringParameters, an object of query string parameters
  * @returns {Promise<ApiResponse>}
  */
 
 module.exports = serializeAxiosResponse(
-  ({ limit, processId, startTime, endTime, deviceId, userId }) => (
-    API.get('log', '', {
-      queryStringParameters: removeUndefinedKeys({
-        limit,
-        processId,
-        startTime,
-        endTime,
-        deviceId,
-        userId,
-      }),
-      ...DEFAULT_REQ_OPTS,
-    })
-  ),
+  (queryStringParameters = {}) => API.get('log', '', {
+    queryStringParameters,
+    ...DEFAULT_REQ_OPTS,
+  }),
 )
