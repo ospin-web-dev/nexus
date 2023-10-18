4e65376855c55fe159de43e29b42550c3d471314
Felix Menzel <Felix.Menzel.Dev@gmail.com>
2023-10-18 14:48:43 +0200

    docs

--- src/licence/type/list.js
+++ src/licence/type/list.js
@@ -7,6 +7,7 @@ const { DEFAULT_REQ_OPTS } = require('../../utils/defaultReqOpts')
  * @desc lists all licence types
  * @memberof nexus.licence.type
  * @function list
+ * @param object the query string parameters, defaults to none
  * @async
  * @returns {Promise<ApiResponse>}
  */
